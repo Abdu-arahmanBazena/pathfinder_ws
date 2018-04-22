6f8ea01c6384ceb4ab4fe2bdb07052547b1fdaa4
Abdu-arahmanBazena <abduarahman.bazena@gmail.com>
2018-04-22 04:57:08 +0200

    update

--- src/path_finder/src/arduinoListener.py
+++ src/path_finder/src/arduinoListener.py
@@ -4,40 +4,56 @@ from path_finder.msg import ard
 from std_msgs.msg import Float64MultiArray
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "ult_right %s", data.ult_right)
-    rospy.loginfo(rospy.get_caller_id() + "ult_left %s", data.ult_left)
-    rospy.loginfo(rospy.get_caller_id() + "ir_right %s", data.ir_right)
-    rospy.loginfo(rospy.get_caller_id() + "ir_left %s", data.ir_left)
-    rospy.loginfo(rospy.get_caller_id() + "seq %s", data.seq)
-    rospy.loginfo(rospy.get_caller_id() + "time %s", data.stamp)
+    #rospy.loginfo(rospy.get_caller_id() + "ult_right %s", data.ult_right)
+    #rospy.loginfo(rospy.get_caller_id() + "ult_left %s", data.ult_left)
+    #rospy.loginfo(rospy.get_caller_id() + "ir_right %s", data.ir_right)
+    #rospy.loginfo(rospy.get_caller_id() + "ir_left %s", data.ir_left)
+    #rospy.loginfo(rospy.get_caller_id() + "seq %s", data.seq)
+    #rospy.loginfo(rospy.get_caller_id() + "time %s", data.stamp)
+    #log the msg to the terminal
+    rospy.loginfo(data)
     infraredPub( data.ir_right , data.ir_left)
-    #ultrasonicPub(data.ult_right , data.ult_left ,data.servo_angle)
-    #compassPub(data.compass_x , data.compass_y , data.compass_z )
-    #compass_x
-    #compass_y
-    #compass_z
-    #servo_angle
+    ultrasonicPub(data.ult_right , data.ult_left ,data.servo_angle)
+    compassPub(data.compass_x , data.compass_y , data.compass_z )
+
 def infraredPub(ir_right , ir_left):
-
+    #decalre the msg
     ir_msg = Float64MultiArray()
-
+    #decalre the publisher
     pub = rospy.Publisher('/ir_raw_readings', Float64MultiArray, queue_size=10)
-
-
-
-    ir_msg.data = [ir_right , ir_left] 
-
-
-
+    #add the data to the msg
+    ir_msg.data = [ir_right , ir_left]
+    #log the msg to the terminal
     rospy.loginfo(ir_msg)
-
+    #publish the msg
     pub.publish(ir_msg)
 
 
-#def ultrasonicPub(ult_right , ult_left , servo_angle):
-#    pub = rospy.Publisher('/ultrasonic_servo_angle', Float64MultiArray, queue_size=10)
-#def compassPub(x , y , z):
-#    pub = rospy.Publisher('/compass_raw_readings', Float64MultiArray, queue_size=10)
+def ultrasonicPub(ult_right , ult_left , servo_angle):
+    #decalre pub msg
+    ult_srv_msg = Float64MultiArray()
+    #declare pub
+    pub = rospy.Publisher('/ultrasonic_servo_angle', Float64MultiArray, queue_size=10)
+    #add data to the msg
+    ult_srv_msg.data = [ult_right , ult_left , servo_angle]
+    #log the msg to the terminal
+    rospy.loginfo(ult_srv_msg)
+    #publish the msg
+    pub.publish(ult_srv_msg)
+
+
+def compassPub(x , y , z):
+    #decalre the msg
+    compass_msg = Float64MultiArray()
+    #declare the Publisher
+    pub = rospy.Publisher('/compass_raw_readings', Float64MultiArray, queue_size=10)
+    #add the data to the msg
+    compass_msg = [x , y , z]
+    #log the msg to the terminal
+    rospy.loginfo(compass_msg)
+    #publish the msg
+    pub.publish(Float64MultiArray ,compass_msg)
+
 
 def listener():
     rospy.init_node('arduino_readings_listener', anonymous=True)
---
+++ src/path_finder/src/ultrasonic_sensors_servo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import String
+def callback(data):
+    #log the received msg to the terminal
+    rospy.loginfo(data)
+    #get the data from the msg
+    x = data.data[0]
+    y = data.data[1]
+    z = data.data[2]
+    #pass the data to the function that works out the direction
+def getDirections(x , y , z):
+    #get directions logic
+    #publish the results
+    #decalre the msg
+    direction_msg = String()
+    #decalre the publisher
+    pub = rospy.Publisher('/ult_srv_NF', Float64MultiArray, queue_size=10)
+    #add the data to the msg
+    direction_msg.data = "North"
+    #log the msg to the terminal
+    rospy.loginfo(direction_msg)
+    #publish the msg
+    pub.publish(direction_msg)
+def listener():
+    rospy.init_node('ultrasonic_sensors_servo', anonymous=True)
+    rospy.Subscriber("/ultrasonic_servo_angle", Float64MultiArray, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
